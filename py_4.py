548962acdff6bc6f10876dabf42a204299fa7e7e
vasumg <vasumg@gmail.com>
2025-12-04 10:47:54 +0000

    Just added 2 more files

---
+++ py_4.py
@@ -0,0 +1,20 @@
+my_string = "This is a string I am using this time"
+split = my_string.split()
+upper = my_string.upper()
+lower = my_string.lower()
+title = my_string.title()
+count = my_string.count("this time")
+print (f"This is the split string:", split)
+print (f"This is the upper string:", upper)
+print (f"This is the lower string:", lower)
+print (f"This is the title string:", title)
+print (f"This is the count string:", count)
+
+basic_string = "Brazilian Jiu Jitsu"
+result = basic_string.split()
+print (f"The split result is:", result)
+length = len(result)
+print (f"The Length of result is:", length)
+string_with_hyphen = "Yanka-Nani-Sheena"
+string_with_hyphen.split("-")
+#print (f"The result of string split:", hyphen-split)
---
+++ py_5.py
@@ -0,0 +1,5 @@
+basic_string = "This is my moment"
+items = ["-", 1,2,3]
+for item in items:
+    basic_string += str(item)
+print (f"The Basic String is:", basic_string)
